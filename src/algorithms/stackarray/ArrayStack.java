fb0dca8627ce3ace546c2271fe770a2946c9dc34
VITALIY KAVUN <kavun.v@dbbest.com>
2021-02-09 16:54:25 +0200

    added stack implementation, based on the array

--- src/algorithms/stackarray/ArrayStack.java
+++ src/algorithms/stackarray/ArrayStack.java
@@ -1,6 +1,7 @@
 package algorithms.stackarray;
 
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 
 public class ArrayStack {
     private Employee[] stack;
@@ -16,5 +17,37 @@ public class ArrayStack {
             System.arraycopy(stack, 0, newEmployeeArr, 0, stack.length);
             stack = newEmployeeArr;
         }
+        stack[top++] = employee;
     }
+
+    public Employee pop(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        Employee employee = stack[--top];
+        stack[top] = null;
+        return employee;
+    }
+
+    public Employee peek(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        return stack[top - 1];
+    }
+
+    public boolean isEmpty(){
+        return top == 0;
+    }
+
+    public int size(){
+        return top;
+    }
+
+    public void printStack(){
+        for (int i = top - 1; i >= 0; i--) {
+            System.out.println(stack[i]);
+        }
+    }
+
 }
--- src/algorithms/stackarray/Main.java
+++ src/algorithms/stackarray/Main.java
@@ -2,6 +2,19 @@ package algorithms.stackarray;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayStack arrayStack = new ArrayStack(10);
 
+        arrayStack.push(new Employee("Tom", "Jackson", 23));
+        arrayStack.push(new Employee("Danny", "Mythack", 245));
+        arrayStack.push(new Employee("Ally", "Mayson", 224));
+        arrayStack.push(new Employee("Jennifer", "Satclif", 37));
+        arrayStack.push(new Employee("End", "End", 28));
+
+        arrayStack.printStack();
+        System.out.println("-----------------");
+        System.out.println(arrayStack.peek());
+        System.out.println("-----------------");
+        System.out.println("Popped: " + arrayStack.pop());
+        System.out.println(arrayStack.peek());
     }
 }
